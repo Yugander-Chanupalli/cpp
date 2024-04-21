3e269079c65e18d5a011978f3409cca9ae0801f0
Chanupalli Yugander <131588735+Yugander-Chanupalli@users.noreply.github.com>
2024-04-21 07:55:58 +0530

    Create Hr_1-Variable sized arrays.cpp

---
+++ Basic_array_problems/Hr_1-Variable sized arrays.cpp
@@ -0,0 +1,45 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    
+    int n,q;
+    cin>>n>>q;
+    
+    vector<vector<int>> vectors(n);
+    
+    for(int i=0; i<n; i++){
+        
+        int size;
+        cin>>size;
+        vector<int> temp;
+        
+        for(int j=0; j<size; j++){
+            int element;
+            cin >> element;
+            temp.push_back(element);    
+        }
+        vectors[i]=temp;
+    
+    }
+   
+    for(int i=0; i<q; i++){
+        int index,pos;
+        cin>> index >> pos;
+    
+        vector<int> updated_vec=vectors[index];
+        if (index>=0 && index<n && pos>=0 && pos<updated_vec.size()) {
+            cout<< updated_vec[pos] << endl;
+        }
+        else{
+            cout<< "check index";
+        }
+    }
+    
+    return 0;
+}
